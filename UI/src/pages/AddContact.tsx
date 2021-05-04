2d0e4867fa65b9ebf874f2664846dc339d885f9f
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-05-04 13:07:49 +0200

    add page title

--- UI/src/pages/AddContact.tsx
+++ UI/src/pages/AddContact.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import {IonHeader,IonButton, IonPage, IonToolbar, IonIcon, IonBackButton, } from '@ionic/react';
-
+import {
+  IonHeader,
+  IonButton,
+  IonPage,
+  IonToolbar,
+  IonIcon,
+  IonBackButton,
+  IonTitle,
+} from "@ionic/react";
 
 const NewContact: React.FC = () => (
-
-    <IonPage>
-        <IonHeader>
-            <IonToolbar>
-            <IonButton fill="outline">
-            <IonBackButton defaultHref="/" />
-            </IonButton>
-            </IonToolbar>
-        </IonHeader>
-    </IonPage>
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonButton fill="outline" className="ion-float-left ion-margin">
+          <IonBackButton defaultHref="/" />
+        </IonButton>
+        <IonTitle className="ion-text-center ion-padding">New Contact</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+  </IonPage>
 );
 
 export default NewContact;
