1eda8163f181c43368b9ff754a8e4cac8650b012
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-05-04 13:58:42 +0200

    add phone input field

--- UI/src/pages/AddContact.tsx
+++ UI/src/pages/AddContact.tsx
@@ -7,7 +7,15 @@ import {
   IonToolbar,
   IonBackButton,
   IonTitle,
+  IonList,
+  IonItem,
+  IonInput,
+  IonContent,
+  IonLabel,
+  IonItemGroup,
+  IonIcon,
 } from "@ionic/react";
+import { add, remove } from "ionicons/icons";
 
 const NewContact: React.FC = () => (
   <IonPage>
@@ -19,6 +27,38 @@ const NewContact: React.FC = () => (
         <IonTitle className="ion-text-center ion-padding">New Contact</IonTitle>
       </IonToolbar>
     </IonHeader>
+    <IonContent>
+      <form action="" className="ion-margin">
+        <IonList slot="center">
+          <IonItem>
+            <IonLabel position="floating">First Name </IonLabel>
+            <IonInput type="text" required></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Last Name </IonLabel>
+            <IonInput type="text" required></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">email </IonLabel>
+            <IonInput type="email" required></IonInput>
+          </IonItem>
+          <IonItemGroup>
+            <IonItem>
+              <IonButton slot="start" size="small" color="danger"  >
+                <IonIcon slot="icon-only" icon={remove} color="light" />
+              </IonButton>
+              <IonInput required></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonButton fill="clear">
+                <IonIcon icon={add} slot="icon-only" />
+                Add phone
+              </IonButton>
+            </IonItem>
+          </IonItemGroup>
+        </IonList>
+      </form>
+    </IonContent>
   </IonPage>
 );
 
