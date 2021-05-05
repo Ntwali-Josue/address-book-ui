504bb840a585289c188c2643adab60bd51dc0c2d
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-05-05 11:37:49 +0200

    update phone and email inputs

--- UI/src/pages/AddContact.tsx
+++ UI/src/pages/AddContact.tsx
@@ -14,8 +14,10 @@ import {
   IonLabel,
   IonItemGroup,
   IonIcon,
+  IonAlert,
+  IonLoading
 } from "@ionic/react";
-import { add, remove } from "ionicons/icons";
+import { add } from "ionicons/icons";
 import { RouteComponentProps } from "react-router";
 
 const NewContact: React.FC<RouteComponentProps> = (props) => {
@@ -23,6 +25,35 @@ const NewContact: React.FC<RouteComponentProps> = (props) => {
   const [lastName, setLastName] = useState("");
   const [phone, setPhone] = useState([""]);
   const [email, setEmail] = useState([""]);
+
+
+  const onPhoneChange = (val: string, i: number) => {
+    if (val.length > 0) {
+      setPhone(
+        phone.map((value, index) => {
+          if (index === i) {
+            return val;
+          } else {
+            return value;
+          }
+        })
+      );
+    }
+  };
+
+  const onEmailChange = (val: string, i: number) => {
+    if (val.length > 0) {
+      setEmail(
+        email.map((value, index) => {
+          if (index === i) {
+            return val;
+          } else {
+            return value;
+          }
+        })
+      );
+    }
+  };
 
   return (
     <IonPage>
@@ -41,53 +72,65 @@ const NewContact: React.FC<RouteComponentProps> = (props) => {
           <IonList slot="center">
             <IonItem>
               <IonLabel position="floating">First Name </IonLabel>
-              <IonInput type="text" required value={firstName} onIonChange={(e) => setFirstName((e.target as HTMLInputElement).value)}></IonInput>
+              <IonInput
+                type="text"
+                required
+                value={firstName}
+                onIonChange={(e) =>
+                  setFirstName((e.target as HTMLInputElement).value)
+                }
+              ></IonInput>
             </IonItem>
             <IonItem>
               <IonLabel position="floating">Last Name </IonLabel>
-              <IonInput type="text" required value={lastName} onIonChange={(e) => setLastName((e.target as HTMLInputElement).value)}></IonInput>
+              <IonInput
+                type="text"
+                required
+                value={lastName}
+                onIonChange={(e) =>
+                  setLastName((e.target as HTMLInputElement).value)
+                }
+              ></IonInput>
             </IonItem>
             <IonItemGroup>
+              {email.map((email, index) => (
+                <IonItem key={`email${index}`}>
+                  <IonLabel position="floating">Email</IonLabel>
+                  <IonInput
+                    required
+                    type="email"
+                    pattern="email"
+                    value={email}
+                    onIonChange={(e) =>
+                      onEmailChange((e.target as HTMLInputElement).value, index)
+                    }
+                  ></IonInput>
+                </IonItem>
+              ))}
               <IonItem>
-                <IonLabel position="floating">email </IonLabel>
                 <IonButton
-                  slot="start"
-                  size="small"
-                  color="danger"
-                  className="ion-float-left ion-margin-top"
+                  fill="clear"
+                  onClick={(e) => setEmail([...email, ""])}
                 >
-                  <IonIcon slot="icon-only" icon={remove} color="light" />
-                </IonButton>
-                <IonInput required type="email" pattern="email"></IonInput>
-              </IonItem>
-              <IonItem>
-                <IonButton fill="clear">
                   <IonIcon icon={add} slot="icon-only" />
                   Add Email
                 </IonButton>
               </IonItem>
 
-              <IonItem>
-                <IonLabel position="floating">phone </IonLabel>
-                <IonButton
-                  slot="start"
-                  size="small"
-                  color="danger"
-                  className="ion-float-left ion-margin-top"
-                >
-                  <IonIcon slot="icon-only" icon={remove} color="light" />
-                </IonButton>
-                <IonInput required type="email"></IonInput>
+            {phone.map((phone, index) => (
+              <IonItem key={`phone${index}`}>
+                <IonLabel position="floating">Phone </IonLabel>
+                <IonInput required type="number" onClick={ (e) => onPhoneChange((e.target as HTMLInputElement).value, index)}></IonInput>
               </IonItem>
+            ))}
               <IonItem>
-                <IonButton fill="clear">
+                <IonButton fill="clear" onClick={(e) => setPhone([...phone, ""])}>
                   <IonIcon icon={add} slot="icon-only" />
                   Add phone
                 </IonButton>
               </IonItem>
             </IonItemGroup>
           </IonList>
-
           <IonButton className="ion-margin-top"> Done</IonButton>
         </form>
       </IonContent>
